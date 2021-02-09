40027fbc16dd0a46822e5c217cfd1e5a69914f04
Enigma Swan Adams <eswanadams@alaska.edu>
2021-02-08 23:28:07 -0900

    edited some output formatting

--- HW-02/main.cpp
+++ HW-02/main.cpp
@@ -28,9 +28,10 @@ of rice grains
 #include "names.h"
 #include "money.h"
 #include "rice.h"
+#include <string> // not sure why but this seamed to fix something 
 
 int main() {
-	/*vector <string> names(0); // for testing the printNames function
+	vector <string> names(0); // for testing the printNames function
 	InputNames(names);
 
 	cout << endl;
@@ -56,10 +57,10 @@ int main() {
 		cout << "I couldn't find it. The name doesn't exist in your list.";
 	}
 	
-	
+	cout << endl;
 	
 	money(); // testing the additional program 1, money.cpp
-	
+	cout << endl;
 
 	///*for (int i = 1; i <= 9; i++) { // for testing 
 		//findNumSquares(i);
@@ -68,16 +69,16 @@ int main() {
 	//cout << powersOf2(3); //  for testing
 
 	int numRice = 0;
-	cout << "Enter how much rice yu would want if you were the inventor: ";
+	std:: cout << "Enter how much rice yu would want if you were the inventor: ";
 	cin >> numRice;
 	int numSquares = findNumSquares(numRice);
 	cout << endl;
-	cout << "You would need to have invented a chess buard with " << numSquares << " squares";
-	*/
+	cout << "You would need to have invented a chess buard with " << numSquares << " squares" << endl;
+	
 
 	//numCalcs(); // just for helping me test what the largest values were 
 	cout << "The largest number of squares for which you can calcualte an\n"
-		"exact number of grains is 31" << endl;
+		"exact number of grains using int is 31" << endl;
 	cout << "The largest number of squares for which you can calcualte an\n"
 		"approximate number of grains using a double is 1024" << endl;
 
